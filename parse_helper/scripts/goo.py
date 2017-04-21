c230467ab489fe2c0055a9d03d5f0007f86c4100
Ken <kenjyco@gmail.com>
2017-04-20 23:11:46 -0500

    Update ph-goo to pass selected links to 'download_file'

--- parse_helper/scripts/goo.py
+++ parse_helper/scripts/goo.py
@@ -1,7 +1,19 @@
+import os.path
 import click
 import input_helper as ih
 import parse_helper as ph
 from pprint import pprint
+
+
+def lazy_filename(text):
+    """
+    - http://stackoverflow.com/a/7406369
+    """
+    return "".join([
+        c
+        for c in text
+        if c.isalpha() or c.isdigit() or c in (' ', '-', '_', '+', '.')
+    ]).rstrip().replace(' ', '-')
 
 
 @click.command()
@@ -12,15 +24,23 @@ def main(query):
     if not query:
         return
 
+    session = ph.new_requests_session()
     selected = ih.make_selections(
-        ph.google_serp(query),
+        ph.google_serp(query, session=session),
         wrap=False,
         item_format='{title} .::. {link}',
     )
-    if selected:
-        ph.logger.info('Selected {}'.format(' '.join([x['link'] for x in selected])))
-        with open(ph.LOGFILE, 'a') as fp:
-            pprint(selected, fp)
+    for item in selected:
+        remote_basename = os.path.basename(item['link'])
+        ext = remote_basename.split('.')[-1]
+        localfile = ''
+        if ext != remote_basename:
+            localfile = lazy_filename(item['title']) + '.' + ext
+        else:
+            localfile = lazy_filename(
+                item['link'].split('://')[-1].strip('/').replace('/', '--')
+            ) + '.html'
+        ph.download_file(item['link'], localfile, session=session)
 
 
 if __name__ == '__main__':
